1d1b95b6613a48e5c3664a2c7ac7330252fe7609
Bryan <bradcede@espol.edu.ec>
2020-12-18 12:48:07 -0500

    Bug real Time resuelto

--- src/app/components/cuenta/cuenta.component.ts
+++ src/app/components/cuenta/cuenta.component.ts
@@ -16,87 +16,87 @@ export class CuentaComponent implements OnInit {
   @ViewChild('emailBox') emailBox: ElementRef;
   @ViewChild('nombreBox') nombreBox: ElementRef;
 
-  public usuario:{id:string,email:string,nombre:string,img:string} = {
+  public usuario: { id: string, email: string, nombre: string, img: string } = {
     id: '',
     email: '',
     nombre: '',
     img: ''
   };
-  public stringApi:String = environment.ApirestImagen;
+  public stringApi: String = environment.ApirestImagen;
 
-  private fileSelected:File = undefined;
+  private fileSelected: File = undefined;
 
-  constructor(private routeActive:ActivatedRoute,
-              private _servicioService:ServicioService,
-              private _imagenServer:ImageService
-              ) {
+  constructor(private routeActive: ActivatedRoute,
+    private _servicioService: ServicioService,
+    private _imagenServer: ImageService
+  ) {
     this.cargaInicial();
-   }
+  }
 
 
-  cargaInicial(){
+  cargaInicial() {
     this.routeActive.params
-    .subscribe((params:{id:string}) =>{
-      this._servicioService.getDatosSimplesWithHeader(environment.ApirestUsers,String(params.id))
-      .subscribe((usuarioDb:{ok:boolean,usuarios:[{_id:string,email:string,nombre:string,img:string}]})=>{
-        this.usuario = {
-          id: usuarioDb.usuarios[0]._id,
-          email: usuarioDb.usuarios[0].email,
-          nombre: usuarioDb.usuarios[0].nombre,
-          img: usuarioDb.usuarios[0].img
-        }
+      .subscribe((params: { id: string }) => {
+        this._servicioService.getDatosSimplesWithHeader(environment.ApirestUsers, String(params.id))
+          .subscribe((usuarioDb: { ok: boolean, usuarios: [{ _id: string, email: string, nombre: string, img: string }] }) => {
+            this.usuario = {
+              id: usuarioDb.usuarios[0]._id,
+              email: usuarioDb.usuarios[0].email,
+              nombre: usuarioDb.usuarios[0].nombre,
+              img: usuarioDb.usuarios[0].img
+            }
+          })
       })
-    })
   }
 
   ngOnInit(): void {
-   
+
   }
 
-  ngAfterViewInit(){
+  ngAfterViewInit() {
   }
-  
-  onFileSelected(event){
+
+  onFileSelected(event) {
     this.fileSelected = <File>event.target.files[0];
     console.log(this.fileSelected);
   }
 
-  onUpload(){
-    if(!this.fileSelected){
+  onUpload() {
+    if (!this.fileSelected) {
       this.actualizarData();
-    }else{
-      const fd:FormData = new FormData();
-      fd.append('archivo',this.fileSelected,this.fileSelected.name);
-      this._imagenServer.uploadImage(fd,this.usuario.id)
-      .subscribe((resp:{ok:boolean,message:string})=>{
-        alert(resp.message);
-        this.actualizarData();
-      })
+    } else {
+      const fd: FormData = new FormData();
+      fd.append('archivo', this.fileSelected, this.fileSelected.name);
+      this._imagenServer.uploadImage(fd, this.usuario.id)
+        .subscribe((resp: { ok: boolean, message: string }) => {
+          alert(resp.message);
+          this.actualizarData();
+        })
     }
-    
+
   }
 
-  actualizarData(){
-    if(this.passBox.nativeElement.value.trim() !== '' || (this.passBox.nativeElement.value.trim().length > 5)){
-      this._imagenServer.actualizarDatos(this.usuario.id,{
+  actualizarData() {
+    if (this.passBox.nativeElement.value.trim() !== '' || (this.passBox.nativeElement.value.trim().length > 5)) {
+      this._imagenServer.actualizarDatos(this.usuario.id, {
         email: this.emailBox.nativeElement.value,
         pass: this.passBox.nativeElement.value,
         nombre: this.nombreBox.nativeElement.value
-      }).subscribe((res)=>{
+      }).subscribe((res) => {
         console.log(res);
         this.cargaInicial();
       })
-    }else{
-      this._imagenServer.actualizarDatos(this.usuario.id,{
+    } else {
+      this._imagenServer.actualizarDatos(this.usuario.id, {
         email: this.emailBox.nativeElement.value,
         nombre: this.nombreBox.nativeElement.value
-      }).subscribe((res)=>{
+      }).subscribe((res) => {
         console.log(res);
         this.cargaInicial();
-        
+
       })
     }
-    
+
   }
-  
+
 }
--- src/app/components/mapa/mapa.component.ts
+++ src/app/components/mapa/mapa.component.ts
@@ -26,27 +26,25 @@ export class MapaComponent {
   public unoHours: Boolean = false;
   public dosHours: Boolean = true;
   public tresHours: Boolean = false;
-  public evento:string = 'recargar';
-  public coorden:{
+  public evento: string = 'recargar';
+  public coorden: {
     "type": "Feature",
-    "properties": {"mag": number,"name":string},
+    "properties": { "mag": number, "name": string },
     "geometry": {
       "type": "Point",
-      "coordinates": [number,number]
+      "coordinates": [number, number]
     }
   }[] = [];
-  public consulta:Consulta;
-  public anioIni:Number;
+  public consulta: Consulta;
+  public anioIni: Number;
   public actualElement = new Map();
   public configMapa: ConfigureRest = new ConfigureRest();
-  constructor(public _servicioService:ServicioService,
-              public _webSocket:WebSocketService,
-              public _servicioConfig:ServicioConfigService
-              ) 
-  
-  {
-    this._servicioConfig.consultarData().subscribe((resp:ConfigResponse)=>{
-      if(resp.ok){
+  constructor(public _servicioService: ServicioService,
+    public _webSocket: WebSocketService,
+    public _servicioConfig: ServicioConfigService
+  ) {
+    this._servicioConfig.consultarData().subscribe((resp: ConfigResponse) => {
+      if (resp.ok) {
         this.configMapa = new ConfigureRest(
           resp.config.latcentro,
           resp.config.longcentro,
@@ -55,41 +53,48 @@ export class MapaComponent {
           resp.config.longini,
           resp.config.latfin,
           resp.config.escala
-          );
+        );
       }
       //console.log(this.configMapa);
       this.consulta = new Consulta();
-      this._webSocket.listendEvent(this.evento).subscribe((res:{lat:number,long:number,_id?:string})=>{
-        if(this.realTime === true){
-          let index:number = 0;
-          for(let coordenada of this.coorden){
-            if(coordenada.properties.name === res._id){
-              break;
-            }
-            index++;
-          }
-          if(this.coorden.length === index){
+      this._webSocket.listendEvent('userOnline').subscribe((res: { id: string, online: boolean }) => {
+        if (!res.online) {
+          let index: number = this.busquedaRealTime(res.id);
+          console.log(index);
+          //console.log(this.coorden);
+          this.coorden.splice(index,1);
+          //console.log(this.coorden);
+          (this.mapa.getSource('earthquakes') as mapboxgl.GeoJSONSource).setData({
+            "type": "FeatureCollection",
+            "features": this.coorden
+          });
+        }
+      })
+      this._webSocket.listendEvent(this.evento).subscribe((res: { lat: number, long: number, _id?: string }) => {
+        if (this.realTime === true) {
+          let index: number = this.busquedaRealTime(res._id);
+          if (this.coorden.length === index) {
             this.coorden.push(
               {
                 "type": "Feature",
-                "properties": {"mag": this.configMapa.escala,"name":res._id},
+                "properties": { "mag": this.configMapa.escala, "name": res._id },
                 "geometry": {
                   "type": "Point",
-                  "coordinates": [res.long,res.lat]
+                  "coordinates": [res.long, res.lat]
                 }
               }
             );
-          }else{
-            this.coorden[index].geometry.coordinates = [res.long,res.lat];
+          } else {
+            this.coorden[index].geometry.coordinates = [res.long, res.lat];
           }
-        }else{
+        } else {
           this.coorden.push(
             {
               "type": "Feature",
-              "properties": {"mag": this.configMapa.escala,"name":"no-real"},
+              "properties": { "mag": this.configMapa.escala, "name": "no-real" },
               "geometry": {
                 "type": "Point",
-                "coordinates": [res.long,res.lat]
+                "coordinates": [res.long, res.lat]
               }
             }
           );
@@ -101,98 +106,109 @@ export class MapaComponent {
       })
       this.cargarCheck(2);
     })
-    
+
   }
 
-  generarConsulta(horasMenos:number,diasMenos?:number):Consulta{
+  busquedaRealTime(id: string) {
+    let index: number = 0;
+    for (let coordenada of this.coorden) {
+      if (coordenada.properties.name === id) {
+        break;
+      }
+      index++;
+    }
+    return index;
+  }
+
+  generarConsulta(horasMenos: number, diasMenos?: number): Consulta {
     let fecha = new Date()
-    if(!diasMenos){
+    if (!diasMenos) {
       let _consulta = new Consulta(
         fecha.getFullYear(),
         fecha.getMonth(),
         fecha.getDate() - (diasMenos || 0),
-        fecha.getHours()- horasMenos - environment.horaDesf
+        fecha.getHours() - horasMenos - environment.horaDesf
       )
-      console.log(_consulta);
+      //console.log(_consulta);
       return _consulta;
-    }else{
+    } else {
       let _consulta = new Consulta(
         fecha.getFullYear(),
         fecha.getMonth(),
         fecha.getDate() - (diasMenos || 0)
       )
-      console.log(_consulta);
+      //console.log(_consulta);
       return _consulta;
     }
   }
 
-  cargarCheck(numero:number){
-    switch(numero){
+  cargarCheck(numero: number) {
+    switch (numero) {
       case 1:
         this.realTime = false;
-        this.cargarDatos(this.generarConsulta(0,1))
-        .then((resp)=>{
-          //console.log(resp)
-          this.cargarMapa();
-        })
+        this.cargarDatos(this.generarConsulta(0, 1))
+          .then((resp) => {
+            //console.log(resp)
+            this.cargarMapa();
+          })
         break;
       case 2:
         this.realTime = false;
         this.cargarDatos(this.generarConsulta(6))
-        .then((resp)=>{
-          //console.log(resp)
-          this.cargarMapa();
-        })
+          .then((resp) => {
+            //console.log(resp)
+            this.cargarMapa();
+          })
         break;
       case 3:
         this.realTime = false;
         this.cargarDatos(this.generarConsulta(1))
-        .then((resp)=>{
-          //console.log(resp)
-          this.cargarMapa();
-        })
+          .then((resp) => {
+            //console.log(resp)
+            this.cargarMapa();
+          })
         break;
       default:
         this.realTime = true;
         this.cargarDatos()
-        .then((resp)=>{
-          //console.log(resp)
-          this.cargarMapa();
-        })
+          .then((resp) => {
+            //console.log(resp)
+            this.cargarMapa();
+          })
         break;
     }
-    
+
   }
 
 
-  cargarDatos(consulta?:Consulta){
-    return new Promise((resolve,reject)=>{
+  cargarDatos(consulta?: Consulta) {
+    return new Promise((resolve, reject) => {
       this.coorden = [];
-      if(consulta){
-        this._servicioService.getDatosJson(environment.Apirest,consulta).subscribe((res:any)=>{
-          res.usuarios.forEach((elemento)=>{
+      if (consulta) {
+        this._servicioService.getDatosJson(environment.Apirest, consulta).subscribe((res: any) => {
+          res.usuarios.forEach((elemento) => {
             this.coorden.push(
               {
                 "type": "Feature",
-                "properties": {"mag": this.configMapa.escala,"name":"no-real"},
+                "properties": { "mag": this.configMapa.escala, "name": "no-real" },
                 "geometry": {
                   "type": "Point",
-                  "coordinates": [elemento.long,elemento.lat]
+                  "coordinates": [elemento.long, elemento.lat]
                 }
               }
             );
           })
         });
-      }else{
-        this._servicioService.getDatosSimplex(environment.ApirestReal).subscribe((res:{ok:boolean,datos:{_id:string,lat:number,long:number}[]})=>{
-          res.datos.forEach((elemento)=>{
+      } else {
+        this._servicioService.getDatosSimplex(environment.ApirestReal).subscribe((res: { ok: boolean, datos: { _id: string, lat: number, long: number }[] }) => {
+          res.datos.forEach((elemento) => {
             this.coorden.push(
               {
                 "type": "Feature",
-                "properties": {"mag": this.configMapa.escala,"name":elemento._id},
+                "properties": { "mag": this.configMapa.escala, "name": elemento._id },
                 "geometry": {
                   "type": "Point",
-                  "coordinates": [elemento.long,elemento.lat]
+                  "coordinates": [elemento.long, elemento.lat]
                 }
               }
             );
@@ -201,18 +217,18 @@ export class MapaComponent {
       }
       resolve(this.coorden);
     })
-    
+
   }
 
-  cargarMapa(){
-    this.mapa = new mapboxgl.Map({ 
+  cargarMapa() {
+    this.mapa = new mapboxgl.Map({
       accessToken: environment.claveMapbox,
       container: 'mapa-mapbox', // container id
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [this.configMapa.longcentro, this.configMapa.latcentro], // starting position
       zoom: 15 // starting zoom
     })
-    this.mapa.on('load',()=>{
+    this.mapa.on('load', () => {
       this.mapa.addSource('earthquakes', {
         type: 'geojson',
         data: {
@@ -233,8 +249,8 @@ export class MapaComponent {
             0,
             6,
             1
-            ],
-          'heatmap-color': ['interpolate',['linear'],['heatmap-density'],
+          ],
+          'heatmap-color': ['interpolate', ['linear'], ['heatmap-density'],
             0,
             'rgba(33,102,172,0)',
             0.2,
@@ -247,7 +263,7 @@ export class MapaComponent {
             'rgb(239,138,98)',
             1,
             'rgb(178,24,43)'
-            ]
+          ]
         }
       }, 'waterway-label')
     })
